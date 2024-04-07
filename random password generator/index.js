2bee83fd761ad806663eb749dcb575cd46a4d596
Vaughan Baloyi <129795438+vaughan23@users.noreply.github.com>
2024-04-07 16:11:50 +0200

    Add files via upload

---
+++ random password generator/index.js
@@ -0,0 +1,29 @@
+// RANDOM PASSWORD GENERATOR
+
+
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    let charset = '';
+    let password = '';
+
+    if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeNumbers) charset += '0123456789';
+    if (includeSymbols) charset += '!@#$%^&*()_+{}[]|;:,.<>?';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
